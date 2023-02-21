0303ba2a22ab504bc676fc32c4c998214f06b972
Yaroslav Miskov <yapyga3@gmail.com>
2023-02-21 21:51:06 +0200

    Message duplicated

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,6 +7,11 @@ namespace Task1
         static void Main(string[] args)
         {
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
         }
     }
 }
