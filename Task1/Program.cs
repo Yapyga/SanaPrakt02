c25d13c11edfe57c3fe8ca18c0f09c550b8e5f43
Yaroslav Miskov <yapyga3@gmail.com>
2023-02-21 19:55:39 +0200

    Message changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
         }
     }
 }
